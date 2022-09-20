99eaaa6787e446e599f984188d25c4c1c099eb0e
Nikolay Zdravkov <unsafeptr@gmail.com>
2022-09-20 21:17:49 +0300

    - add net6 LoggerMessageAttribute to benchmarks

--- src/LoggingBenchmark/LoggingBenchmarks.cs
+++ src/LoggingBenchmark/LoggingBenchmarks.cs
@@ -3,18 +3,20 @@ using BenchmarkDotNet.Jobs;
 
 namespace LoggingBenchmark;
 
-[SimpleJob(RuntimeMoniker.Net462)]
-[SimpleJob(RuntimeMoniker.Net472)]
-[SimpleJob(RuntimeMoniker.Net48)]
-[SimpleJob(RuntimeMoniker.Net50)]
+//[SimpleJob(RuntimeMoniker.Net462)]
+//[SimpleJob(RuntimeMoniker.Net472)]
+//[SimpleJob(RuntimeMoniker.Net48)]
+//[SimpleJob(RuntimeMoniker.Net50)]
 [SimpleJob(RuntimeMoniker.Net60, baseline: true)]
+[MemoryDiagnoser]
 public class LoggingBenchmarks
 {
 	readonly Services.DirectILoggerService _directILoggerService = new();
 	readonly Services.ExtensionBasedLoggerMessageService _extensionLoggerMessageService = new();
 	readonly Services.InterfaceBasedLoggerMessageService _interfaceLoggerMessageService = new();
+	readonly Services.LoggerMessageServiceLogger _loggerMessageServiceViaLoggerMessageAttribute = new();
 
-	[Params(1, 10, 100)]
+	[Params(10)]
 	public int Iterations { get; set; }
 
 	[Benchmark(Baseline = true, Description = "Direct:ILogger<T>")]
@@ -41,6 +43,15 @@ public class LoggingBenchmarks
 		for (var i = 0; i < Iterations; i++)
 		{
 			_interfaceLoggerMessageService.Execute();
+		}
+	}
+
+	[Benchmark(Description = "PartialClassGenerated:LoggerMessage")]
+	public void LoggerViaLoggerMessageAttributeService()
+	{
+		for (var i = 0; i < Iterations; i++)
+		{
+			_loggerMessageServiceViaLoggerMessageAttribute.Execute();
 		}
 	}
 }
