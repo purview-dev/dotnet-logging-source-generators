d5c9a5d9e5c0c1c6d59a0afca13d7f25b7509880
Nikolay Zdravkov <unsafeptr@gmail.com>
2022-09-21 18:30:49 +0300

    - add message template to TestStart for IServiceLogger

--- src/LoggingBenchmark/Services/IServiceLogger.cs
+++ src/LoggingBenchmark/Services/IServiceLogger.cs
@@ -7,5 +7,6 @@ interface IServiceLogger
 	[LogEvent(Level = LogLevel.Error, Message = LoggingBenchmarkConsts.TestErrorMessage)]
 	void TestError(string? stringParameter, int? intParameter);
 
+	[LogEvent(Message = LoggingBenchmarkConsts.TestStartMessage)]
 	IDisposable TestStart(DateTimeOffset started);
 }
